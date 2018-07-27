370cff4441eb7835919d86b413f72760f8e10f06
ColetteVois <c.voisembert@gmail.com>
2018-07-27 20:07:07 +0200

    essai for upload data in the app

--- load_data/load_data_2.R
+++ load_data/load_data_2.R
@@ -1,10 +1,11 @@
-load.data.2 <- function() {
+load.data.2 <- function(lien) {
   
+  lien <- gsub("\\","/", lien, fixed=TRUE)
   # twitter <- read.csv("C:/Users/rubik/Desktop/Intership_NLP_CU/Document/Twitter-Data/Twitter-Data/#cancer+smoking.csv", sep=",", encoding = "UTF-8", header = FALSE, col.names = "text", stringsAsFactors = FALSE)
-  twitter <- read.csv("C:/Users/Projet/Desktop/Internship/Data/Twitter-Data/#cancer+smoking.csv", sep=",", encoding = "UTF-8", header = FALSE, col.names = "text", stringsAsFactors = FALSE)
-  twitter2 <- read.csv("C:/Users/Projet/Desktop/Internship/Data/Twitter-Data/cancer+smoking.csv", sep=",", encoding = "UTF-8", header = FALSE, col.names = "text", stringsAsFactors = FALSE)
+  twitter <- read.csv(lien, sep=",", encoding = "UTF-8", header = FALSE, col.names = "text", stringsAsFactors = FALSE)
+  #twitter2 <- read.csv("C:/Users/Projet/Desktop/Internship/Data/Twitter-Data/cancer+smoking.csv", sep=",", encoding = "UTF-8", header = FALSE, col.names = "text", stringsAsFactors = FALSE)
   
-  original_books <- as_data_frame(dplyr::bind_rows(twitter,twitter2))
+  original_books <- as_data_frame(twitter)    #dplyr::bind_rows(twitter,twitter2))
   #original_books <- as.character(original_books1)
 
   if (DEBUG == TRUE) {original_books}
